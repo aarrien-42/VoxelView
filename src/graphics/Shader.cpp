be0b551f37721ba2adc7718f0602ecd0684fb924
Andoni Arrien <andoniarrien27@gmail.com>
2024-12-09 21:27:39 +0100

    Expanded Shader class functionality, now can set vec4 uniforms

--- src/graphics/Shader.cpp
+++ src/graphics/Shader.cpp
@@ -81,7 +81,11 @@ void Shader::Unbind() const {
 }
 
 GLint Shader::GetUniformLocation(const std::string& name) const {
-	return glGetUniformLocation(m_id, name.c_str());
+	GLint location = glGetUniformLocation(m_id, name.c_str());
+	if (location == -1) {
+		std::cerr << "Error: Uniform " << name << " not found." << std::endl;
+	}
+	return location;
 }
 
 void Shader::SetUniform(const std::string& name, int value) const {
@@ -94,6 +98,10 @@ void Shader::SetUniform(const std::string& name, float value) const {
 
 void Shader::SetUniform(const std::string& name, const glm::vec3& value) const {
 	glUniform3fv(GetUniformLocation(name), 1, &value[0]);
+}
+
+void Shader::SetUniform(const std::string& name, const glm::vec4& value) const {
+	glUniform4fv(GetUniformLocation(name), 1, &value[0]);
 }
 
 void Shader::SetUniform(const std::string& name, const glm::mat4& value) const {
