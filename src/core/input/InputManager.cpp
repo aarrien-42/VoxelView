cb33446596ef540106fdb631c42f79d641262af9
Andoni Arrien <andoniarrien27@gmail.com>
2024-12-16 20:57:59 +0100

    Updated InputManager Update method for clarity

--- src/core/input/InputManager.cpp
+++ src/core/input/InputManager.cpp
@@ -12,20 +12,24 @@ InputManager::~InputManager()
 
 void InputManager::Update()
 {
+	// Poll window events and handle input for all registered agents
 	m_window->PollEvents();
 
+	// Handle mouse input
 	double x = 0.0, y = 0.0;
 	m_window->GetCursorPos(x, y);
-	for (size_t j = 0; j < m_agents.size(); j++) {
-		m_agents[j]->HandleMouseInput(x, y);
+	for (IControl* agent : m_agents) {
+		agent->HandleMouseInput(x, y);
 	}
 
+	// Handle keyboard input
 	float deltaTime = m_window->GetDeltaTime();
 	std::vector<int>keys = m_keyBindings.GetBindKeys();
-	for (size_t i = 0; i < keys.size(); i++) {
-		if (m_window->GetKeyState(keys[i]) == GLFW_PRESS) {
-			for (size_t j = 0; j < m_agents.size(); j++) {
-				m_agents[j]->HandleKeyboardInput(m_keyBindings.GetKeyAction(keys.at(i)), deltaTime);
+	for (int key : keys) {
+		if (m_window->GetKeyState(key) == GLFW_PRESS) {
+			for (IControl* agent : m_agents) {
+				Action action = m_keyBindings.GetKeyAction(key);
+				agent->HandleKeyboardInput(action, deltaTime);
 			}
 		}
 	}
